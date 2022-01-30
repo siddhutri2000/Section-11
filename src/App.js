2983ecad9c3c817c44f82c49f064f1be3e81627f
sid <siddhu.tri2000@gmail.com>
2022-01-30 16:18:51 +0530

    used callBacks to deal with the non premitive data type comparision caused using REACT.memo function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
 import Button from "../src/components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("app running");
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}></DemoOutput>
+      <DemoOutput show={showParagraph}></DemoOutput>
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}> Toggle paragraph</Button>
     </div>
   );
