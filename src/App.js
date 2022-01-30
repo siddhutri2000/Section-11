aca42d9fbfa7576b6909424ce5c25c97fb1f947e
sid <siddhu.tri2000@gmail.com>
2022-01-30 14:59:09 +0530

    added a button and the logic to change the state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Button from "../src/components/UI/Button/Button";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("app running");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}> Toggle paragraph</Button>
     </div>
   );
 }
